0cd2768632bb87c83cce407f990d69e8248dcb38
Tony <gm10285@gmail.com>
2021-06-30 22:26:43 +0800

    add windows

---
+++ windows/install_all.bat
@@ -0,0 +1,4 @@
+choco install vcredist2013 -y
+choco install postgresql13 --params '/Password:123456' -y
+choco install openjdk8 -y
+choco install postman -y
---
+++ windows/install_jdk8.ps1
@@ -0,0 +1,1 @@
+choco install openjdk8 -y
